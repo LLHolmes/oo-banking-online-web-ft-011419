2cf65c2b50e897c1405fd94db30f8fa722373243
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-29 16:28:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+  
+  def intitialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
 end
